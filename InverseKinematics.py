2bedf5aaf52b0c8b1ee4ce13dcb23ae02e36039f
R0ner <79334550+R0ner@users.noreply.github.com>
2021-04-07 15:05:22 +0200

    updated inversekinematics

--- InverseKinematics.py
+++ InverseKinematics.py
@@ -33,79 +33,58 @@ def set_goal(goal_joints, pose):
         goal[goal_joint] = dummy_joints[goal_joint].coordinate
     return goal
 
+class Inverse_model:
+    def __init__(self, prior = (None, None), excluded=None, saveframes = True, plot=False):
+        self.prior_type, self.prior_model = prior
+        self.excluded = excluded
+        self.children, self.indices = exclude(excluded, return_indices=True)
+        self.saveframes = saveframes
+        self.frames = []
+        self.plot = plot
+        self.result = None
 
-def inverse_kinematics(goal, prior = (None,), excluded=None, saveframes = False, plot=False):
-    prior_type, prior_model = prior
+    def stitch(self, optim_joints):
+        pose = []
+        idx = 0
+        for i in range(59):
+            if i in self.indices:
+                pose.append(optim_joints[idx])
+                idx += 1
+            else:
+                pose.append(torch.tensor([0.], requires_grad=False))
+        return torch.cat(pose)
 
-    frames = []
+    def inverse_kinematics(self, goal, lr=1, n_epochs=100):
+        #saveframes = self.saveframes
+        frames = []
+        #prior_type, prior_model = self.prior_type, self.prior_model
 
-    dummy_joints, dummy_pose, dof = dummy(return_dof=True)
-    dummy_joints['root'].set_motion(dummy_pose)
+        joints, dummy_pose, dof = dummy(return_dof=True)
+        joints['root'].set_motion(dummy_pose)
 
-    # We exclude some joints for optimisation
-    children, indices = exclude(excluded, return_indices=True)
+        # initialize as tensor of zeros
+        optim_joints = [torch.tensor([0.], requires_grad=True) for i in range(np.size(self.indices))]
 
-    # initialize as tensor of zeros
-    optim_joints = [torch.tensor([0.], requires_grad=True) for i in range(np.size(indices))]
+        optimizer = optim.Adam(optim_joints, lr=lr)
 
-    lr = 1
-    n_epochs = 100
-    optimizer = optim.Adam(optim_joints, lr=lr)
+        y = [y for y in goal.values()]
 
-    y = [y for y in goal.values()]
+        for epoch in range(n_epochs+1):
+            def closure():
+                optimizer.zero_grad()
 
-    for epoch in range(n_epochs+1):
-        if plot and not epoch % 10:
-            print(epoch)
-            dummy_joints['root'].draw(list(goal.values())[0].numpy())
+                pose = self.stitch(optim_joints)
+                pose_dict = array2pose(pose)
 
-        def closure():
-            optimizer.zero_grad()
+                joints['root'].set_motion(pose_dict)
+                yhat = [joints[goal_joint].coordinate for goal_joint in goal.keys()]
 
-            #model outpout
-            # pose = [torch.zeros(3).float()]
-            pose = []
-            idx = 0
-            for i in range(59):
-                if i in indices:
-                    pose.append(optim_joints[idx])
-                    idx += 1
-                else:
-                    pose.append(torch.tensor([0.], requires_grad=False))
+                if self.saveframes:
+                    frames.append(pose_dict)
 
-            pose = torch.cat(pose)
-            pose_dict = array2pose(pose)
-            # print(pose)
-            dummy_joints['root'].set_motion(pose_dict)
-            yhat = [dummy_joints[goal_joint].coordinate for goal_joint in goal.keys()]
-
-            if saveframes:
-                frames.append(pose_dict)
-
-            if prior_type == None:
-                loss = Loss_Euclidian(yhat, y)
-            elif prior_type == 'normal':
-                loss = Loss_Normal(pose[indices], yhat, y, prior_model, 1e-2)
-            print(loss)
-            loss.backward(retain_graph=True)
-            return loss
-        optimizer.step(closure)
-
-        # with torch.no_grad():
-        #     for child in children:
-        #         limits = dummy_joints[child].limits
-        #         for
-            # for child in children:
-            #     count = 0
-            #     for lim in range(3):
-            #         if dummy_joints[child].limits[lim].any():
-            #             OptimJoints[count].clamp_(dummy_joints[child].limits[lim][0], dummy_joints[child].limits[lim][1])
-            #             count += 1
-
-    print(optim_joints)
-    print(children)
-    if saveframes:
-        return frames
-if __name__ == '__main__':
-    A = 123
-    # inverse_kinematics(goal_name, goal_coord)
+                loss = Loss(yhat, y, self.prior_model, self.prior_type, pose[self.indices])
+                loss.backward(retain_graph=True)
+                return loss
+            optimizer.step(closure)
+        self.result = optim_joints
+        self.frames = frames
